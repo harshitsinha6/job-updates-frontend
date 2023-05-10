8db3cbcebd07b1a8cc0f991879f2b2cdd174afed
Harshit Sinha <harshitsinha6@gmail.com>
2023-05-10 05:48:21 +0530

    added

--- src/app/component/view-details/view-details.service.ts
+++ src/app/component/view-details/view-details.service.ts
@@ -8,8 +8,8 @@ import { Observable } from 'rxjs';
 })
 export class ViewDetailsService {
 
-  filteredURI: any = "http://44.193.9.15:8181/api/get-filtered-jobs/";
-  URI: any = "http://44.193.9.15:8181/api/get-jobs/";
+  filteredURI: any = "http://3.227.12.172:8181/api/get-filtered-jobs/";
+  URI: any = "http://3.227.12.172:8181/api/get-jobs/";
 
   constructor(private http: HttpClient) { }
 
